30bb30ae1d770e7001d7c69db87682aae4df4cbc
anuj2authbridge <anuj.mishra@authbridge.com>
2016-01-22 18:37:07 +0530

    Create UsersTable.php

---
+++ cake3/Model/UsersTable.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+class UsersTable extends Table
+{
+
+    public function validationDefault(Validator $validator)
+    {
+        return $validator
+            ->notEmpty('fname', 'A firstname is required')
+            ->notEmpty('lname', 'A last name is required')
+            ->notEmpty('email', 'email is required')
+            ->notEmpty('password', 'A password is required') 
+			 ->add('password', 'passwordsEqual', [
+				'rule' => function ($value, $context) {
+				return
+					isset($context['data']['cpassword']) &&
+					$context['data']['cpassword'] === $value;},
+				'message'=>'Password not matched'	
+				])
+            ->notEmpty('tnc', 'You must accept our terms of service');
+
+    }
+    public function beforeFilter(Event $event)
+    {
+        $this->Auth->allow(['login', 'regisstration']);
+    }	
+
+}
