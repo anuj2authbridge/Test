a255ba13f60d4d9929eec50b3633bafc2da9d739
anuj2authbridge <anuj.mishra@authbridge.com>
2016-02-22 17:22:57 +0530

    Create nojavascript.php

---
+++ nojavascript.php
@@ -0,0 +1,8 @@
+<noscript>
+        &lt;div class="overlay"&gt;&lt;/div&gt;
+        &lt;div class="modaljs"&gt;
+            &lt;h2 style="color: #0b68b2; font-size: 30px; padding: 36px 10px; width: 100%; text-align: center"&gt;JavaScript is either disabled or not supported by your browser.&lt;br /&gt;
+                This site requires JavaScript to function properly.&lt;/h2&gt;
+            &lt;div style="margin: 0 auto; width: 100%; font-size: 20px"&gt;&lt;a href="http://www.enable-javascript.com/" class="returnHome" target="_blank" title="Click Here how to enable javaScript"&gt;Click here how to enable javaScript&lt;/a&gt;&lt;/div&gt;
+        &lt;/div&gt;
+    </noscript>
