dcc5bc2cb723a0656a66beb01712223bf76a45ec
anuj2authbridge <anuj.mishra@authbridge.com>
2016-04-11 15:54:50 +0530

    Create index.php

---
+++ nodejs/index.php
@@ -0,0 +1,27 @@
+installation
+SPEP 1-: download the node js package (http://nodejs.org/dist/node-v0.1.96.tar.gz)
+
+STEP 2-: Extract folder
+run following command
+STEP 3-: cd /home/anuj/Downloads/node-v0.1.96
+
+STEP 4-:  ./configure
+
+STEP 5-: make
+
+STEP 6-: sudo make install
+
+
+Now check node version by command -: node -v
+
+other option 
+
+download by terminal
+
+1) wget http://nodejs.org/dist/node-v0.1.96.tar.gz
+2) gunzip node-v0.1.96.tar.gz
+3) tar -xf node-v0.1.96.tar
+4) cd node-v0.1.96
+5) ./configure
+6) make
+7) sudo make install
