b2903b43c42c8ccef968f01b97083c5362e505d5
anuj2authbridge <anuj.mishra@authbridge.com>
2016-04-12 13:59:58 +0530

    Update form.php

--- cakephp/form.php
+++ cakephp/form.php
@@ -15,26 +15,28 @@ echo $this->Form->input('Test.input_name',array('type'=>'text', 'id'=>'input_id'
 // input type select
 
 <?php											
-		echo $this->Form->input('Test.input_name',array('type'>'select', 'id'=>'input_id', 
-		'options'=>$options, 'empty'=>'-Select-', 'label'=>false, 'legend'=>false, 
-		'div'=>false, 'hiddenField'=>false));
-			
-		?>
-		
-		// end of form
-		<?php echo $this->Form->end();?>
-		
-		--------------------------
-		
-		// link 
-		<?php 
-							echo $this->Html->link(
-							$this->Html->image('excel_icon.gif'),
-							array(
-							'controller' => 'test', // controller name
-							'action' => 'index/export:csv?'.$exportExcell,  //action name
-							'full_base' => true
-							),
-							array(
-							'escape'=>false,'style'=>'margin-right:5px;', 'title'=>'Export'));
-						?>
+echo $this->Form->input('Test.input_name',array('type'>'select', 'id'=>'input_id', 
+'options'=>$options, 'empty'=>'-Select-', 'label'=>false, 'legend'=>false, 
+'div'=>false, 'hiddenField'=>false));
+
+?>
+
+// end of form
+<?php echo $this->Form->end();?>
+
+--------------------------
+
+// link 
+<?php 
+echo $this->Html->link(
+$this->Html->image('excel_icon.gif'),
+array(
+'controller' => 'test', // controller name
+'action' => 'index/export:csv?'.$exportExcell,  //action name
+'full_base' => true
+),
+array(
+'escape'=>false,'style'=>'margin-right:5px;', 'title'=>'Export'));
+?>
+****************************** Impotr file ************************
+ App::import('Vendor', 'excel', array('file' => 'excel' . DS . 'excelwriter.inc.php'));
