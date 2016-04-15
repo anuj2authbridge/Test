c215efb72d6ccba2a1d07acfb3e6bcfdaa8a0044
anuj2authbridge <anuj.mishra@authbridge.com>
2016-04-15 13:02:15 +0530

    Create intall_php_pear_mail.php

---
+++ phpmail/intall_php_pear_mail.php
@@ -0,0 +1,23 @@
+In Ubuntu:
+
+sudo apt-get install php-pear
+
+sudo pear install mail
+
+sudo pear install Net_SMTP
+
+sudo pear install Auth_SASL
+
+sudo pear install mail_mime
+
+In Centos:
+
+Yum install php-pear
+
+pear install mail
+
+pear install Net_SMTP
+
+pear install Auth_SASL
+
+pear install mail_mime
