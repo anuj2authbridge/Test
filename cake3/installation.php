8fb49f4d7f8b21b52aa96fe65c1fe5763c7a29a3
anuj2authbridge <anuj.mishra@authbridge.com>
2017-04-03 14:19:24 +0530

    Create installation.php

---
+++ cake3/installation.php
@@ -0,0 +1,12 @@
+Installation 
+
+Step 1 -:  apt-get install php-intl
+
+Step 2 -: apt-get install php-mbstring
+
+Step 3-: install composer curl -sS https://getcomposer.org/installer | php
+
+change directory (cd /opt/lampp/htdocs/)
+
+Step 4 -: php /composer.phar create-project --prefer-dist cakephp/app cake3
+
