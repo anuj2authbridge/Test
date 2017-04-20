cdc3a46a20343b9afa2098ebec7724fcefe9af70
anuj2authbridge <anuj.mishra@authbridge.com>
2017-04-20 09:46:12 +0530

    Update User.php

--- cake3/Entity/User.php
+++ cake3/Entity/User.php
@@ -5,8 +5,11 @@ use Cake\ORM\Entity;
 
 class User extends Entity
 {
-    // Make all fields mass assignable for now.
-    protected $_accessible = ['*' => true];
+    // Make all fields mass assignable except for primary key field "id".
+    protected $_accessible = [
+        '*' => true,
+        'id' => false
+    ];
 
    /* 
       protected function _setPassword($password)
