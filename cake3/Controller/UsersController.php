51f0393c2e3f7e4e8f47ae47b590b10e5a88bee0
anuj2authbridge <anuj.mishra@authbridge.com>
2017-04-20 09:34:04 +0530

    Update UsersController.php

--- cake3/Controller/UsersController.php
+++ cake3/Controller/UsersController.php
@@ -1,75 +1,88 @@
 <?php
 namespace App\Controller;
+use App\Controller\AppController;
 use Cake\Event\Event;
+use Cake\Auth\DefaultPasswordHasher;
+use Cake\Datasource\ConnectionManager;
+use Cake\ORM\TableRegistry;
 class UsersController extends AppController
 {
-
-   public function beforeFilter(Event $event)
+    public function beforeFilter(Event $event)
     {
-        $this->Auth->allow(['login','registration']);
-    } 
-	public function registration()
-	{
-	$this->layout='login_default';	
-        $user = $this->Users->newEntity();
+        parent::beforeFilter($event);
+        $this->Auth->allow(array('login','index', 'add'));
+    }
+
 	
-        if ($this->request->is('post'))
-		{
+public function login()
+    {
+        $this->title_for_layout = "User Login";
+        $this->viewBuilder()->layout('outer');
+        $this->loadComponent('Cookie', ['expiry' => '2 day']);
+	   if ($this->request->is('post'))		{
 			
-        $user = $this->Users->patchEntity($user, $this->request->data);
-        if ($this->Users->save($user))
-			 {
-                $this->Flash->success(__('The user has been saved.'));
-                return $this->redirect(array('action' => 'login'));
-             } 
-            $this->Flash->error(__('Unable to add user.'));  
-        }
-        $this->set('user', $user);	
+           if ( $this->Auth->identify()) 
+			{
+                $this->Auth->setUser($user);
+				$user_form_data=$this->request->data;
+				//pr($this->Auth->User());
+				if($this->Auth->User('USER_STATUS')==0)
+				{
+					$this->Flash->error(__('Your login account has been disabled. Kindly confirm with your TL.'));	
+					return $this->redirect($this->Auth->logout());
+				}
+				if($user_form_data['persistent']==1)
+				{
+					$this->Cookie->configKey('User', 'encryption', false);
+					$this->Cookie->write('User',
+						['USER_LOGINNAME' => $user_form_data['USER_LOGINNAME'], 'USER_PASSWORD' => $user_form_data['USER_PASSWORD'],'USER_STATUS'=>$this->Auth->User('USER_STATUS')]
+					);
+				}
+				$UsersTable=TableRegistry::get('UserLoginActivities'); 
+				$User=$UsersTable->newEntity();
 
-	}
-	public function login()
-	{
-          $this->layout='login_default';	
-		//$2y$10$JuKfjrqTAlldyQdWu/SJl.C3PW3pn/FsUSi2Y3HnIO1
-		//pr($this->request->data);		
-    if ($this->request->is('post')) {
-        $user = $this->Auth->identify();
-        if ($user) {
-            $this->Auth->setUser($user);
-            return $this->redirect(array('controller'=>'Dashboards','action'=>'index'));
+				$User->USER_ID=$this->Auth->User('USER_ID');
+				$User->LOGIN_DATE=date('Y-m-d H:i:s');
+				$User->LOGIN_ACTION=1;
+				$User->LOGIN_STATUS=1;
+				$User->IP_ADDRESS=$this->request->clientIp();
+				$UsersTable->save($User);
+               return $this->redirect($this->Auth->redirectUrl());
+            }
+            $this->Flash->error(__('Invalid username or password, try again'));
         }
-        $this->Flash->error('Your username or password is incorrect.');
-    }		
-}
-	public function logout()
-	{
-		$this->Flash->success('You are now logged out.');
-		return $this->redirect($this->Auth->logout());
-	}
-	public function edit()
-	{
-	    $pro_data = $this->Users->get($this->Auth->user('id'));
-		if ($this->request->is(['post', 'put'])) 
+		else
 		{
-			$this->Users->patchEntity($pro_data,$this->request->data);
-			if ($this->Users->save($pro_data)) {
-				$this->Flash->success(__('Your article has been updated.'));
-				return $this->redirect(array('action' => 'edit'));
-			}
-			$this->Flash->error(__('Unable to update your article.'));
+			$this->Cookie->configKey('User', 'encryption', false);
+				$user=$this->Auth->User();
+				if(!empty($user))
+				{
+					return $this->redirect($this->Auth->redirectUrl());
+				}
+                else if($this->Cookie->check('User.USER_ID') && $this->Cookie->check('User.USER_STATUS')!=0)
+				{
+					$this->request->data['USER_LOGINNAME']=$this->Cookie->read('User.USER_LOGINNAME');
+					$this->request->data['USER_PASSWORD']=$this->Cookie->read('User.USER_PASSWORD');
+					$user = $this->Auth->identify($this->request->data);
+					if($user)
+					{
+						$this->Auth->setUser($user);
+			    	    return $this->redirect($this->Auth->redirectUrl());
+					}
+				}					
+			   else
+			   {
+				  $this->Cookie->delete('User');
+			   } 
+			   //$this->Cookie->read('User');die;
 		}
-
-    $this->set('pro_data', $pro_data);	
-		
-	}
-	public function view()
-	{
-		$view_profile_data=$this->Users->find('all')
-										->first();			
-		$this->set('view_profile_data',$view_profile_data);						
-        }
-
-
-
-/*End*/	
+    }
+	
+public function logout()
+    {
+		$session = $this->request->session();
+		$session->destroy();
+		//$this->Flash->success(__('Loged out successfully'));
+		return $this->redirect($this->Auth->logout());
+    }
 }
